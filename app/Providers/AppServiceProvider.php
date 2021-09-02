10042521a5c8383ed9939bdcf52bd8fc1ee5199e
Chris Rahmé <chrahme@dcgroup.com>
2021-09-02 09:37:33 +0300

    Updated AppServiceProvider to set string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
-        Schema::defaultStringLength(300);
+        Schema::defaultStringLength(191);
     }
 }
