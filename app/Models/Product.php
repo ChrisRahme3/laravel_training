8c3b595bb6eeb1d3a4e8ea379a32584f5ef1b607
Chris Rahmé <chrahme@dcgroup.com>
2021-09-02 10:46:43 +0300

    Created relationship between Category and Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
-class Product extends Model
-{
+class Product extends Model {
     use HasFactory;
+
+    public function category() {
+        return $this->belongsTo('App\Models\Category');
+    }
 }
