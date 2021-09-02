a194b12f1f0676f1520b977c580724c8a346374b
Chris Rahmé <chrahme@dcgroup.com>
2021-09-02 11:23:22 +0300

    Added fillable fields

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,18 @@ use App\Models\Category;
 class Product extends Model {
     use HasFactory;
 
+    protected $fillable = [
+        'id',
+        'name',
+        'description',
+        'features',
+        'price',
+        'keywords',
+        'url',
+        'category_id',
+        'subcategory'
+    ];
+
     public function category() {
         return $this->belongsTo('App\Models\Category');
     }
