0c8e6424e7840f5dc249cbdd2d237947b576b96d
Chris Rahmé <chrahme@dcgroup.com>
2021-09-03 10:28:37 +0300

    Added routes resources

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'categories' => CategoryController::class,
+    'products'   => ProductController::class,
+]);
 
 Route::get('{any}', function() {
     return view('products.index');
