ac0c5f57e054822a2e58cbd39d3ee2b0da099d53
Chris Rahmé <chrahme@dcgroup.com>
2021-09-03 13:36:12 +0300

    Added option to remove ellipsis in `truncate`

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,6 +1,6 @@
 // Capitalize the first letter of every word in `str`
 function toTitle(str = '') {
-    return str.replace(/[^-'\s]+/g, function(word) {
+    return str.replace(/[^-\s]+/g, function(word) {
         return word.replace(/^./, function(first) {
             return first.toUpperCase();
         });
@@ -9,13 +9,19 @@ function toTitle(str = '') {
 
 // Truncate `str` to the last word before `n` characters if `cutLastWord` is false,
 // otherwise it will directly cut at the `n`th characters
-function truncate(str, n, cutLastWord = false) {
+function truncate(str, n, cutLastWord = false, ellipsis = true) {
+    if (n <= 0) {
+        return '';
+    }
+    
     if (str.length <= n) {
         return str;
     }
+    
+    n = ellipsis ? n - 3 : n;
 
-    const subString = str.substr(0, n - 1);
-    const text = cutLastWord ? subString : subString.substr(0, subString.lastIndexOf(" "));
+    const sub  = str.substr(0, n - 1);
+    const text = cutLastWord ? sub : sub.substr(0, sub.lastIndexOf(" "));
 
-    return text + "...";
+    return ellipsis ? text + "..." : text;
 };
