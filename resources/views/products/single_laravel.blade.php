211b4e681cd81d58fdedec7888d1e4801b1ab67a
Chris Rahmé <chrahme@dcgroup.com>
2021-09-07 08:15:57 +0300

    Styling

--- resources/views/products/single_laravel.blade.php
+++ resources/views/products/single_laravel.blade.php
@@ -10,32 +10,41 @@
 @section('content')
     <div>
         <div id="product-page" class="d-flex flex-row">
+            <?php
+                $prodname = $product['name'];
+                $prodprce = $product['price'];
+
+                $category_id = $product['category']['id'];
+                $category    = toTitle($product['category']['name']);
+                $subcategory = toTitle($product['subcategory']);
+                
+                if ($subcategory != $category && !empty($subcategory)) {
+                    $category = $category . ' / ' . $subcategory;
+                }
+
+                $features = rtrim(str_replace('<p>', '', str_replace('</p>', ', ', $product['features'])), ', ');
+                $keywords = toTitle($product['keywords']);
+                $prodcode = $product['code'];
+            ?>
+
             <div id="product-image">
-                <img src="https://via.placeholder.com/900x600" alt="{{ $product['name'] }}">
+                <img src="https://via.placeholder.com/900x600" alt="{{ $prodname }}">
             </div>
 
             <div id="product-info">
                 <div id="product-header">
-                    <h1>{{ $product['name'] }}</h2>
-                    <h2>{{ $product['price'] }} $</h2>
+                    <h1>{{ $prodname }}</h2>
+                    <h2>{{ $prodprce }} $</h2>
                 </div>
 
                 <hr><br>
 
                 <div id="product-description">
-                    <?php
-                        $category    = toTitle($product['category']['name']);
-                        $subcategory = toTitle($product['subcategory']);
-                        
-                        if ($subcategory != $category && !empty($subcategory)) {
-                            $category = $category . ' / ' . $subcategory;
-                        }
-                    ?>
                     <h5>{{ $product['description'] }}</h5>
-                    <h6><b>Category:</b> {{ $category }}</h6>
-                    <h6><b>Features:</b> {{ rtrim(str_replace('<p>', '', str_replace('</p>', ', ', $product['features'])), ', ') }}</h6>
-                    <h6><b>Keywords:</b> {{ toTitle($product['keywords']) }}</h6>
-                    <h6><b>Product Code:</b> {{ $product['code'] }}</h6>
+                    <p><b>Category:</b> <a href="/api/categories/{{ $category_id }}">{{ $category }}</a></p>
+                    <p><b>Features:</b> {{ $features }}</p>
+                    <p><b>Keywords:</b> {{ $keywords }}</p>
+                    <p><b>Product Code:</b> {{ $prodcode }}</p>
                 </div>
             </div>
         </div>  
