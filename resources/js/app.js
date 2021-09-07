146e10618abf0c20a5e4e8dfa56be5e7adc0f56d
Chris Rahmé <chrahme@dcgroup.com>
2021-09-07 12:07:29 +0300

    Installed `vue-async-computed`

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+import AsyncComputed from 'vue-async-computed'
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 
+Vue.use(AsyncComputed);
 Vue.use(VueRouter);
 
 Vue.component('products-index', require('./components/products/Index.vue').default);
