a7a223b32dd90953757b80e053b1810abe166ae5
Chris Rahmé <chrahme@dcgroup.com>
2021-09-09 09:17:09 +0300

    Fixed missing variable

--- resources/js/mixins/productMixin.js
+++ resources/js/mixins/productMixin.js
@@ -1,6 +1,8 @@
 export default {
     computed: {
         product() {
+            let obj = this.$store.state.product;
+
             obj.features_mod = obj.features.replaceAll('<p>', '').replaceAll('</p>', ', ').trim().slice(0, -1);
 
             return obj;
