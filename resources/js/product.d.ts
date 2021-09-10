1b0b0b0abc81e39bb4c2527c0151accc359dc559
Chris Rahmé <chrahme@dcgroup.com>
2021-09-10 10:51:13 +0300

    Create product.d.ts

---
+++ resources/js/product.d.ts
@@ -0,0 +1,13 @@
+export interface Product {
+	category?     : string,
+	category_id?  : number,
+	code?         : string,
+	description   : string,
+	features      : string,
+	features_mod? : string,
+	id            : number,
+	keywords?     : string,
+	keywords_mod? : string,
+	name          : string,
+	price         : number
+}
