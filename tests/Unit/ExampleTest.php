d2c3ea630cb90e13dd62cd764f9e1f12993a99ee
Chris Rahmé <chrahme@dcgroup.com>
2021-09-15 08:34:53 +0300

    "Product belongs to Category" test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,26 +2,20 @@
 
 namespace Tests\Unit;
 
-use App\Http\Controllers\ProductController;
+use App\Models\Category;
 use App\Models\Product;
-use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-// use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase {
 	use RefreshDatabase;
 
-    // public function test_example() {
-    //     $this->assertTrue(true);
-    // }
-
 	public function test_product_factory_works() {
-		$fake_product = Product::factory()->make(
+		$product = Product::factory()->make(
 			['id' => 1000]
 		);
 
-		$this->assertEquals($fake_product->id, 1000);
+		$this->assertEquals($product->id, 1000);
 	}
 
 	public function test_get_product_id() {
@@ -29,5 +23,11 @@ class ExampleTest extends TestCase {
 			$product = json_decode(file_get_contents('http://127.0.0.1:8000/api/products/'.$i));
 			$this->assertEquals($product->id, $i);
 		}
+	}
+
+	public function test_product_belongs_to_category() {
+		$product = Product::factory()->make();
+
+		$this->assertInstanceOf(Category::class, $product->category);
 	}
 }
