41e3e46da545f751b5c3bf9bc234982963d8f9e0
Chris Rahmé <chrahme@dcgroup.com>
2021-09-15 14:14:50 +0300

    Added browser tests

--- tests/Browser/BrowserTest.php
+++ tests/Browser/BrowserTest.php
@@ -2,22 +2,72 @@
 
 namespace Tests\Browser;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class BrowserTest extends DuskTestCase
-{
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+class BrowserTest extends DuskTestCase {
+    use DatabaseMigrations;
+
+    public function test_laravel_single_product_page() {
+        $this->browse(function (Browser $bro) {
+			$product = Product::factory()->create();
+
+            $bro->visit('/product/'.$product->id)
+				->screenshot('test_laravel_single_product_page')
+				->assertSeeLink('Back')
+				->assertSee('Category')
+				->assertSee($product->code);
         });
     }
+
+	public function test_laravel_single_product_page_back_button() {
+		$this->browse(function (Browser $bro) {
+			$product = Product::factory()->create();
+
+            $bro->visit('/product/'.$product->id)
+				->clickLink('Back')
+				->pause(1000)
+				->screenshot('test_laravel_single_product_page_back_button')
+				->assertSee('Products')
+				->assertVisible('#products-index')
+				->assertNotPresent('#products-single')
+				->assertSee($product->name);
+        });
+	}
+
+	public function test_vue_index_product_page_card_button() {
+		$this->browse(function (Browser $bro) {
+			$product = Product::factory()->create();
+
+            $bro->visit('/products/index_vue/')
+				->pause(1000)
+				->click('#product-card-'.$product->id.' button')
+				->screenshot('test_vue_index_product_page_card_button')
+				->assertVisible('#product-single')
+				->assertNotPresent('#products-grid')
+				->assertSee('Category')
+				->assertSee($product->code)
+				->pause(1000)
+				->assertVue('product.name', $product->name);
+        });
+	}
+}
+
+class BrowserTestTemp extends DuskTestCase {
+public function test_vue_index_product_page_card_button() {
+	$this->browse(function (Browser $bro) {
+		$product = Product::factory()->create();
+
+		$bro->visit('/products/index_vue/')
+			->pause(1000)
+			->click('#product-card-'.$product->id.' button')
+			->screenshot('test_vue_index_product_page_card_button')
+			->assertVisible('#product-single')
+			->assertNotPresent('#products-grid')
+			->assertSee('Category')
+			->assertSee($product->code);
+	});
+}
 }
